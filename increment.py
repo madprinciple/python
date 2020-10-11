181d2b844327145c008a82ee12d236c11dc1fefd
IT-Admin <techtalks@blu.com.sg>
2020-10-11 17:33:18 +0800

    learning

---
+++ increment.py
@@ -0,0 +1,6 @@
+print ("This is gonna increment by 2 until 0 to 6")
+for i in range(0, 6, +1):
+    print(i)
+print ("this is gonna decrement by 2 untill 8 to 2")
+for i in range(8, 2, -1):
+    print(i)
---
+++ maths.py
@@ -0,0 +1,10 @@
+a=10
+b=2
+
+c=a/b
+d=b%a
+e=a*b
+f=a+b
+g=a-b
+
+print('answers for the conditions respectively',c, ' ',d, ' ' ,e,' ',f,' ',g)
