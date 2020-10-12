76488a632aaab3a82bb23728299aa159094f7c34
IT-Admin <antonsriyantha@gmail.com>
2020-10-12 21:18:59 +0800

    sample regex

---
+++ regex.py
@@ -0,0 +1,8 @@
+import re
+#check your regex here 
+pattern="^[0-9]+"
+key=input("Enter number")
+if(re.search(pattern,key)):
+    print ("your key in number is", key)
+else:
+    print ("please key in valid number")
